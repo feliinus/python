b992381ed9cdb25455494268801bf8ea88b6f39c
feliinus <immortaliscreatura@protonmail.com>
2022-03-29 11:58:47 +0000

    task 1 completed

---
+++ Lesson1/task_1.py
@@ -0,0 +1,32 @@
+print ('task #1')
+'''
+реализовать вывод информации о промежутке времени в зависимости от его продолжительности duration в секундах
+до минуты: <s> сек;
+до часа: <m> мин <s> сек;
+до суток: <h> час <m> мин <s> сек;
+'''
+# 1 минута
+one_minute = 60
+# 1 час
+one_hour = 3600
+# 1 день
+one_day = 86400
+
+duration = int(input('Ввести время в секундах: '))
+
+#вывод информации до минуты:
+if duration<one_minute:
+    print ('{} сек'.format(duration))
+
+#вывод информации до часа:
+elif one_minute <= duration and one_hour > duration:
+    my_minute=duration//one_minute
+    my_second=duration%one_minute
+    print ('{} мин {} сек'.format(my_minute,my_second));
+#вывод информации до суток:
+elif duration >= one_hour and duration < one_day:
+    my_hour=duration // one_hour
+    duration=duration % one_hour
+    my_minute = duration // one_minute
+    my_second = duration % one_minute
+    print ('{} час {} мин {} сек'.format(my_hour,my_minute,my_second));
