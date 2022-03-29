18803a1a08900e0acf99f6d0237fb19ada976a62
feliinus <immortaliscreatura@protonmail.com>
2022-03-29 15:55:52 +0000

    edited code, found errors in syntax

--- Lesson1/task_1.py
+++ Lesson1/task_1.py
@@ -1,4 +1,4 @@
-print ('task #1')
+print('task #1')
 '''
 реализовать вывод информации о промежутке времени в зависимости от его продолжительности duration в секундах
 до минуты: <s> сек;
@@ -14,19 +14,19 @@ one_day = 86400
 
 duration = int(input('Ввести время в секундах: '))
 
-#вывод информации до минуты:
-if duration<one_minute:
-    print ('{} сек'.format(duration))
+# вывод информации до минуты:
+if duration < one_minute:
+    print('{} сек'.format(duration))
 
-#вывод информации до часа:
-elif one_minute <= duration and one_hour > duration:
-    my_minute=duration//one_minute
-    my_second=duration%one_minute
-    print ('{} мин {} сек'.format(my_minute,my_second));
-#вывод информации до суток:
-elif duration >= one_hour and duration < one_day:
-    my_hour=duration // one_hour
-    duration=duration % one_hour
+# вывод информации до часа:
+elif one_minute <= duration < one_hour:
+    my_minute = duration // one_minute
+    my_second = duration % one_minute
+    print('{} мин {} сек'.format(my_minute, my_second))
+    # вывод информации до суток:
+elif one_hour <= duration < one_day:
+    my_hour = duration // one_hour
+    duration = duration % one_hour
     my_minute = duration // one_minute
     my_second = duration % one_minute
     print ('{} час {} мин {} сек'.format(my_hour,my_minute,my_second));
